eff6452734a9787a06bc6b57f2c1ee6ef5e5a458
João Gabriel Antunes <jg.antunes.medeiros@gmail.com>
2021-03-26 17:51:19 -0300

    Adicionando correcao para comportamento inesperado por nao separar os quadrantes do sudoku.

--- sudoku.pl
+++ sudoku.pl
@@ -51,3 +51,18 @@ sudoku([[A1,B1,C1,D1,E1,F1,G1,H1,I1],
     all_different([G1,G2,G3,G4,G5,G6,G7,G8,G9]),
     all_different([H1,H2,H3,H4,H5,H6,H7,H8,H9]),
     all_different([I1,I2,I3,I4,I5,I6,I7,I8,I9]),
+
+    % adicionando a separacao dos quadrantes do sudoku
+    % 1
+    all_different([A1,A2,A3,B1,B2,B3,C1,C2,C3]),
+    all_different([D1,D2,D3,E1,E2,E3,F1,F2,F3]),
+    all_different([G1,G2,G3,H1,H2,H3,I1,I2,I3]),
+    % 2
+    all_different([A4,A5,A6,B4,B5,B6,C4,C5,C6]),
+    all_different([D4,D5,D6,E4,E5,E6,F4,F5,F6]),
+    all_different([G4,G5,G6,H4,H5,H6,I4,I5,I6]),
+    % 3
+    all_different([A7,A8,A9,B7,B8,B9,C7,C8,C9]),
+    all_different([D7,D8,D9,E7,E8,E9,F7,F8,F9]),
+    all_different([G7,G8,G9,H7,H8,H9,I7,I8,I9]),
+    label(Vars).
